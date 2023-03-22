a28e8012cf76f160387b3ce5d2c6ebe92a40cb42
Artsem V <formulaedm@gmail.com>
2023-03-22 11:34:50 +0100

    Release
    * Add the under construction image
    
    * Add debug bundle
    
    * Add webapp pack
    
    * Set up database and create some entities
    
    * Add admin panel
    
    * Create twig template and pagination
    
    * Configure database sessions
    
    * Add navigation (using event subscriber)
    
    * Add lifecycle events
    
    * Add feedback form
    
    * Secure the Admin panel
    
    * Prevent Spam
    
    * Tests
    
    * Async
    
    * Workflows
    
    * Emails and cache
    
    * Style the site and resize images
    
    * Cron
    
    * Internationalization and notifications
    
    * run tests with github actions
    
    * use mariadb for tests instead of postgres
    
    * GB-8 - Fix the issue with redis (#4)
    
    * GB-8 - Fix the issue with redis
    
    * GB-8 - Fixes
    
    * update session handle id for ci/cd
    
    * add node.js to github actions
    
    * GB-7 - Standardize workflow with env files (#3)
    
    * GB-7 - Standardize workflow with env files
    
    * GB-7 - Add variables to .env.test
    
    * GB-7 - Fix tests
    
    * cache ci/cd assets build
    
    * add auto tag main branch changes
    
    ---------
    
    Co-authored-by: Veranika <veranika.biblis@gmail.com>
    Co-authored-by: veranika107 <54105577+veranika107@users.noreply.github.com>

---
+++ src/Entity/Comment.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $author = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
+    private ?string $text = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Conference $conference = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photoFilename = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $state = 'submitted';
+
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getConference(): ?Conference
+    {
+        return $this->conference;
+    }
+
+    public function setConference(?Conference $conference): self
+    {
+        $this->conference = $conference;
+
+        return $this;
+    }
+
+    public function getPhotoFilename(): ?string
+    {
+        return $this->photoFilename;
+    }
+
+    public function setPhotoFilename(?string $photoFilename): self
+    {
+        $this->photoFilename = $photoFilename;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(?string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
