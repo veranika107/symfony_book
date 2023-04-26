7bfd065430662dad81b93b1052a2884dda26b547
veranika107 <54105577+veranika107@users.noreply.github.com>
2023-04-26 12:22:43 +0200

    GB-18 - Implement social auth via google connect (#19)

---
+++ src/Controller/Api/v1/GoogleAuthenticationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Api\v1;
+
+use App\Service\Google\GoogleUserManager;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\GoogleUser;
+use League\OAuth2\Client\Token\AccessToken;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GoogleAuthenticationController extends AbstractController
+{
+    #[Route('/api/auth/google', name: 'api_auth_with_google', methods: ['POST'])]
+    public function authWithGoogle(
+        ClientRegistry $clientRegistry,
+        Request $request,
+        GoogleUserManager $googleUserManager,
+        JWTTokenManagerInterface $JWTManager,
+        RefreshTokenManagerInterface $refreshTokenManager,
+        AuthenticationSuccessHandler $authenticationSuccessHandler
+    ): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $accessToken = $data ? $data['token'] : null;
+        if (!$accessToken) {
+            return $this->json('Token parameter is missing.', Response::HTTP_BAD_REQUEST);
+        }
+
+        $client = $clientRegistry->getClient('google');
+        try {
+            /** @var GoogleUser $googleUser */
+            $googleUser = $client->fetchUserFromToken((new AccessToken(['access_token' => $accessToken])));
+        } catch (IdentityProviderException $exception) {
+            return $this->json('Access token is invalid.', Response::HTTP_FORBIDDEN);
+        }
+
+        $user = $googleUserManager->getUserFromGoogleUser($googleUser);
+
+        // Create access and refresh token for the user.
+        $appAccessToken = $JWTManager->create($user);
+        $authenticationSuccessHandler->handleAuthenticationSuccess($user, $appAccessToken);
+        $refreshToken = $refreshTokenManager->getLastFromUsername($user->getEmail())->getRefreshToken();
+        return $this->json(['token' => $appAccessToken, 'refresh_token' => $refreshToken]);
+    }
+}
---
+++ tests/Service/Google/GoogleUserManagerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tests\Service\Google;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Service\Google\GoogleUserManager;
+use League\OAuth2\Client\Provider\GoogleUser;
+use PHPUnit\Framework\TestCase;
+
+class GoogleUserManagerTest extends TestCase
+{
+    private GoogleUserManager $googleUserManager;
+
+    private UserRepository $userRepository;
+
+    public function setUp(): void
+    {
+        $this->userRepository = $this->createMock(UserRepository::class);
+
+        $this->googleUserManager = new GoogleUserManager($this->userRepository);
+    }
+
+    public function testCreateUserFromGoogleUserWithExistingUser(): void {
+        $googleUser = new GoogleUser(['email' => 'user@example.com', 'given_name' => 'FirstName', 'family_name' => 'LastName']);
+        $user = new User(email: 'user@example.com', roles: []);
+
+        $this->userRepository->expects($this->once())
+            ->method('findOneBy')
+            ->willReturn($user);
+        $this->userRepository->expects($this->never())
+            ->method('save');
+
+        $existingUser = $this->googleUserManager->getUserFromGoogleUser($googleUser);
+        $this->assertInstanceOf(User::class, $existingUser);
+        $this->assertSame($user, $existingUser);
+    }
+
+    public function testCreateUserFromGoogleUserWithNewUser(): void {
+        $googleUser = new GoogleUser(['email' => 'user@example.com', 'given_name' => 'FirstName', 'family_name' => 'LastName']);
+
+        $this->userRepository->expects($this->once())
+            ->method('findOneBy')
+            ->willReturn(null);
+        $this->userRepository->expects($this->once())
+            ->method('save');
+
+        $user = $this->googleUserManager->getUserFromGoogleUser($googleUser);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame('user@example.com', $user->getEmail());
+        $this->assertSame(['ROLE_USER'], $user->getRoles());
+        $this->assertSame('FirstName', $user->getUserFirstName());
+        $this->assertSame('LastName', $user->getUserLastName());
+    }
+}
